840dd6dd71cb91aa70397582fa8063dd3c099e94
ArashSamani <as.developer1991@gmail.com>
2026-06-27 09:15:49 -0700

    Change main connect to db

--- LibraryProjectPy/Main.py
+++ LibraryProjectPy/Main.py
@@ -6,3 +6,17 @@ from Sv.sv_Book import SvBook
 _newbokk = ModelBook(NAME='c#',TITLE='asp',COUNT=4)
 _service = SvBook()
 _service.Insert(_newbokk)
+
+
+import sqlite3 as sql
+
+con = sql.connect('e:\PROJECT\Py-Project-Library\Mobtakeran\LibraryProjectPy\DataBase\DbLibrary.db')
+
+cur = con.cursor()
+
+
+cur.execute("select * from Books")
+booklist = cur.fetchall()
+print(booklist[0])
+
+con.close()
--- LibraryProjectPy/Models/Model_Book.py
+++ LibraryProjectPy/Models/Model_Book.py
@@ -4,4 +4,5 @@ class ModelBook:
         self.Title = TITLE
         self.Count = COUNT
 
-    
+
+
